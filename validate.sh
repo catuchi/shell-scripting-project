dc9830867553e4a55a17da894343d11500404c4e
Chibu Atuchi <atuchibueze@gmail.com>
2024-07-17 14:20:46 +0000

    validate inputs for hostname and server choice

--- validate.sh
+++ validate.sh
@@ -17,3 +17,36 @@ validate_number_two() {
                 return 1  # Return 1 for false (invalid number)
         fi
 }
+
+validate_hostname() {
+	#Function to validate user input for hostname
+	
+	# Check for empty or whitespace-only input
+	if [[ -z "${1// }" ]]; then
+		echo "Invalid hostname: Input cannot be empty or just spaces."
+                return 1
+        fi
+
+        # Validate hostname format
+        if [[ $1 =~ ^[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$ ]]; then
+                echo "127.0.0.1   $1" | sudo tee -a /etc/hosts > /dev/null
+                return 0
+        else
+                echo "Invalid hostname: Must follow hostname rules."
+                return 1
+        fi
+}
+
+validate_choice() {
+	# Function to check user input for server choice
+	
+        if [ $1 == 1 ]; then
+                server_name="apache2"
+                return 0
+        elif [ $1 == 2 ]; then
+                server_name="nginx"
+                return 0
+        else
+                return 1
+        fi
+}
